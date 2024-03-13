e9f5ffae4d21c1991557970b924db1cf595b2d46
jvill171 <juanvillarreal64@yahoo.com>
2024-03-13 17:29:48 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Define routes
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
